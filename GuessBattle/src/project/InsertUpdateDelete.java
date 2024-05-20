0baa2a826b04d22145ad46b5d449016bc0d65118
Rahat Alfaz Abir <72164850+rahat-abir@users.noreply.github.com>
2024-05-20 23:18:16 +0600

    first commit

---
+++ GuessBattle/src/project/InsertUpdateDelete.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+
+import javax.swing.JOptionPane;
+import java.sql.*;
+/**
+ *
+ * @author rahat
+ */
+public class InsertUpdateDelete {
+    public static void setData(String Query,String msg){
+        Connection con=null;
+        Statement st=null;
+        try{
+            con= ConnectionProvider.getCon();
+            st=con.createStatement();
+            st.executeUpdate(Query);
+            if(!msg.equals("")){
+                JOptionPane.showMessageDialog(null, msg);
+            }
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }finally{
+            try{
+                
+            }catch(Exception e){
+                
+            }
+        }
+    }
+}
